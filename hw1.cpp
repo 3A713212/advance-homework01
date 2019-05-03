32a51fcf3065425a409c3e52c2054645dc769dfc
3A713212 <48340382+3A713212@users.noreply.github.com>
2019-05-03 08:33:03 +0800

    Add files via upload

---
+++ hw1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+
+int main(void) 
+{
+	int i=26;
+	int j;
+	for(j=0;j<i;j++)
+		printf("%c",j+65);
+	    printf("\n"); 
+	for(j=0;j<i;j++)
+	    printf("%c",j+97);
+	return 0;
+}
